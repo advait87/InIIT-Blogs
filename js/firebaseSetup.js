9d6fdaa46c0fc82f6cce7786200d420d2c6d7cfd
rick <rick@Advaits-MacBook-Air-740.local>
2025-08-02 16:54:15 +0530

    Adding and viewing blogs work, completely without css.

--- js/firebaseSetup.js
+++ js/firebaseSetup.js
@@ -1,22 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, firebaseui } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyBJmSPPpF5Wr2K77rVHEVnB-WDlOdYp25A",
-authDomain: "insiit-blogs.firebaseapp.com",
-projectId: "insiit-blogs",
-storageBucket: "insiit-blogs.firebasestorage.app",
-messagingSenderId: "125224944479",
-appId: "1:125224944479:web:f3be4413220cdda129acea"
-};
+	apiKey: "AIzaSyBJmSPPpF5Wr2K77rVHEVnB-WDlOdYp25A",
+	authDomain: "insiit-blogs.firebaseapp.com",
+	projectId: "insiit-blogs",
+	storageBucket: "insiit-blogs.firebasestorage.app",
+	messagingSenderId: "125224944479",
+	appId: "1:125224944479:web:f3be4413220cdda129acea"
+}
 
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = await initializeApp(firebaseConfig)
+export default app
